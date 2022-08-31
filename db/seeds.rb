65ce4fbc41ed6e2b668b3cd0b12236db1303038b
faruk <studentfaruk0308@gmail.com>
2022-08-31 21:08:00 +1000

    31082022 2100

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create([{title: "Second Title", description: "Second Description"}, {title: "Third Title", description: "Third Description"}])
